8517e2abedaafa8019718fc9ee3e2df757cf8e48
kkmonster <kkmonster@hotmail.com>
2014-07-03 20:18:21 +0700

    Renew All Projetcs

---
+++ GccApplication1/GccApplication1/GccApplication1.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 7/2/2014 7:53:28 PM
+ *  Author: Wasin
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
