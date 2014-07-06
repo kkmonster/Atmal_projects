44d05bbdd188b74a58b4f20060b224a6224d08a0
kkmonster <kkmonster@hotmail.com>
2014-07-06 13:34:46 +0700

    add initial parameter

--- Robot_line_follower/Robot_line_follower/Robot_line_follower.ino
+++ Robot_line_follower/Robot_line_follower/Robot_line_follower.ino
@@ -8,26 +8,55 @@
  * Created: 7/6/2014 12:19:59 PM
  * Author: Wasin
  */ 
-int motor_r = 5 ;
-int	motor_l = 6 ;
+unsigned int motor_r		=	5 ;
+unsigned int	motor_l		=	6 ;
+unsigned int sensor_l	=	A0;
+unsigned int sensor_r	=	A2;
 
-int gain_p = 0	;
-int gain_i = 0	;
-int gain_d = 0	;
-int gain_v = 0	;
+unsigned int VR1	= A1 ;
+unsigned int VR2	= A3 ;
+unsigned int VR3	= A5 ;
+unsigned int VR4	= A4 ;
 
-int sensor_l =;
-int sensor_r =;
+unsigned int gain_p = 0	;
+unsigned int gain_i = 0	;
+unsigned int gain_d = 0	;
+unsigned int gain_v = 0	;
+
+
+unsigned int read_buffer (int pin)
+{
+	
+	unsigned int buffer_read = 0;
+
+	for (int index=0; index < 10 ; index++)
+	{
+		delay(50);
+		buffer_read = buffer_read + analogRead(pin);
+	}
+	return buffer_read/10 ;
+}
+
 
 void setup()
 {
 	pinMode(motor_r,OUTPUT);
 	pinMode(motor_l,OUTPUT);
+	
+	gain_p = read_buffer(VR2)  ;
+	gain_i = read_buffer(VR3)	;
+	gain_d = read_buffer(VR4)	;
+	gain_v = read_buffer(VR1)	;
+	
 }
+
+
 
 void loop()
 {
+	int error = analogRead(sensor_l)-analogRead(sensor_r);
+	
+	
 
-	  /* add main program code here, this code starts again each time it ends */
-
+	
 }
